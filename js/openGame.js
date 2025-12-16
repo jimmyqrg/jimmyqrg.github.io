c8e488f7273a0995e4f4762f2748a76207aeb868
RanGua <165354267+jimmyqrg@users.noreply.github.com>
2025-12-16 04:53:40 +0000

    adjust openGame.js so it opens jimmyqrg.github.io/loader.html instead of ../loader.html

--- js/openGame.js
+++ js/openGame.js
@@ -101,7 +101,7 @@ function patchOpenGame() {
   window.openGame = function(url) {
     if (!url) return;
 
-    const target = "/loader.html?content=" + encodeURIComponent(url);
+    const target = "https://jimmyqrg.github.io/loader.html?content=" + encodeURIComponent(url);
 
     const popupFeatures = [
       "popup=yes",
